c8b8431bd0ca1e1c96e687a0130795b03ab34563
Ryan <josephmryan14@gmail.com>
2019-01-28 15:24:13 -0800

    reduced api redundent calls

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,11 +1,25 @@
+import _ from 'lodash';
 import jsonPlaceHolder from '../apis/jsonPlaceHolder';
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+  const userIds = _.uniq(_.map(getState().posts, 'userId'));
+  userIds.forEach(id => dispatch(fetchUser(id)));
+}
 
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceHolder.get('/posts');
   dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
+// This is a function that returns a function that calls _fetchUser
 export const fetchUser = (id) => async dispatch => {
   const response = await jsonPlaceHolder.get(`/users/${id}`);
   dispatch({ type: 'FETCH_USER', payload: response.data });
-}
+};
+
+// export const fetchUser = (id) => dispatch => _fetchUser(id, dispatch);
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceHolder.get(`/users/${id}`);
+//   dispatch({ type: 'FETCH_USER', payload: response.data });
+// });
--- blog/src/components/UserHeader.js
+++ blog/src/components/UserHeader.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchUser } from '../actions';
 
 class UserHeader extends React.Component {
-  componentDidMount() {
-    this.props.fetchUser(this.props.userId);
-  }
   render() {
-    return <div>User Header</div>;
+    const { user } = this.props;
+    if(!user) {
+      return null;
+    }
+    return <div className="header">{ user.name }</div>;
   }
 }
 
-export default connect(null, { fetchUser })(UserHeader);
+const mapStateToProps = (state, ownProps) => {
+  return { user: state.user.find(user => user.id === ownProps.userId) };
+};
+
+export default connect(mapStateToProps)(UserHeader);
