9a4fa6371aefc1d768756470ff447e0db995e0c1
Ryan <josephmryan14@gmail.com>
2019-01-28 18:26:42 -0800

    Added our routers to the app

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={ StreamList }></Route>
+          <Route path="/streams/new" component={ StreamCreate }></Route>
+          <Route path="/streams/edit" component={ StreamEdit }></Route>
+          <Route path="/streams/delete" component={ StreamDelete }></Route>
+          <Route path="/streams/show" component={ StreamShow }></Route>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
