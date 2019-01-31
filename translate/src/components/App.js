640d2b7e03125c89d35f06d7f797ea0661f21299
Ryan <josephmryan14@gmail.com>
2019-01-31 15:59:18 -0800

    Added the baseline to our app file

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
 class App extends React.Component {
+  state = { language: 'english'};
+
+  ongLangaugeChange = (language) => {
+    this.setState({ language });
+  }
 
   render() {
     return(
       <div className="ui container">
-        App
+        <div>
+          Select a language:
+          <i className="flag us" onClick={ () => this.ongLangaugeChange('english') } />
+          <i className="flag nl" onClick={ () => this.ongLangaugeChange('dutch') } />
+        </div>
+        { this.state.language }
       </div>
     );
   }
