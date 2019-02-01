b3d47af3ce62f14090cb9f166532ec1bb5a05ea6
Ryan <josephmryan14@gmail.com>
2019-01-31 16:16:44 -0800

    Added context folder with the languagecontext file

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import UserCreate from './UserCreate';
 
 class App extends React.Component {
   state = { language: 'english'};
@@ -15,7 +16,7 @@ class App extends React.Component {
           <i className="flag us" onClick={ () => this.ongLangaugeChange('english') } />
           <i className="flag nl" onClick={ () => this.ongLangaugeChange('dutch') } />
         </div>
-        { this.state.language }
+        <UserCreate />
       </div>
     );
   }
