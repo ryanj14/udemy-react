4b1f4463e4b613900bf8cd46955f8ffbdaf06ca5
Ryan <josephmryan14@gmail.com>
2019-01-31 17:08:03 -0800

    Added colorcontext to the project

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import UserCreate from './UserCreate';
+import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
 
 class App extends React.Component {
   state = { language: 'english'};
@@ -16,7 +18,11 @@ class App extends React.Component {
           <i className="flag us" onClick={ () => this.ongLangaugeChange('english') } />
           <i className="flag nl" onClick={ () => this.ongLangaugeChange('dutch') } />
         </div>
-        <UserCreate />
+        <ColorContext.Provider value="red">
+          <LanguageContext.Provider value={ this.state.language }>
+            <UserCreate />
+          </LanguageContext.Provider>
+        </ColorContext.Provider>
       </div>
     );
   }
