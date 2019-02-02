29e538035f5a2c8ff504985b9aaaff69c097aee5
Ryan <josephmryan14@gmail.com>
2019-02-01 17:50:44 -0800

    Added hooks to the project and made app a functional component instead

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,33 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonsDisplay from './SeasonDisplay';
 import Spinner from './Spinner';
+import useLocation from './useLocation';
 
-class App extends React.Component {
-  state = { lat: null , errorMessage: ''};
-
-  componentDidMount() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => this.setState({ lat: position.coords.latitude }),
-      (err) => this.setState({ errorMessage: err.message })
-    );
+const App = () => {
+  const [lat, errorMessage] = useLocation();
+  let content;
+  if(errorMessage) {
+    return <div>Error: { errorMessage }</div>;
+  } else if(lat) {
+    content = <SeasonsDisplay lat={ lat } />;
+  } else {
+    content = <Spinner message="Please accept location request"/>;
   }
-
-  renderContent() {
-    if(this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage}</div>;
-    } else if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonsDisplay lat={this.state.lat}/>;
-    } else {
-      return <Spinner message="Please accept location request"/>;
-    }
-  }
-
-  render() {
-    return <div className="border red">
-    {this.renderContent()}
-    </div>
-  }
-}
+  return <div className="border red">{ content }</div>
+};
 
 ReactDOM.render(<App />, document.querySelector('#root'));
 
